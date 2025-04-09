9c88676d50460834c70314d4bfd05eeead6eafe4
tms-nguyenvu <vu.nguyen1@tomosia.com>
2025-04-09 15:56:47 +0700

    feat: turbo_stream comment

--- rails-vite/app/controllers/comments_controller.rb
+++ rails-vite/app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @editing_comment_id = params[:editing_comment_id]  
+    @editing_comment_id = params[:editing_comment_id]
     @comments = @post.comments.includes(:user)
   end
 
@@ -18,27 +18,42 @@ class CommentsController < ApplicationController
     end
 
     if @comment.update(comment_params)
-      redirect_to post_path(@post), notice: "Comment has been updated"
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to post_path(@post), notice: "Comment has been updated" }
+      end
     else
       redirect_to post_path(@post, editing_comment_id: @comment.id), alert: "Failed to update comment"
     end
   end
+
 
   def create
     @comment = @post.comments.create(comment_params)
     @comment.user = current_user
 
     if @comment.save
-      redirect_to post_path(@post), notice: 'Comment has been created'
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to post_path(@post), notice: "Comment created" }
+      end
     else
-      redirect_to post_path(@post), alert: 'Comment has not been created'
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("comment_form", partial: "comments/shared/form",
+        locals: { post: @post, comment: @comment }) }
+        format.html { redirect_to post_path(@post), alert: "Failed to create comment" }
+      end
     end
   end
+
 
   def destroy
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post)
+    respond_to do |format|
+      format.html { redirect_to post_path(@post), notice: "Comment deleted" }
+      format.turbo_stream
+    end
   end
 
   private
