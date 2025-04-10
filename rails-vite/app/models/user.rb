8875da2cfc35ffb18eac2b78d8502ee09f4c3e7f
tms-nguyenvu <vu.nguyen1@tomosia.com>
2025-04-10 10:51:50 +0700

    feat: active job integration

--- rails-vite/app/models/user.rb
+++ rails-vite/app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+
+
+  # Override for use with Active Job
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
