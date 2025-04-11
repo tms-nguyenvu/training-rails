ecf4e205bf0f4ba13f93ce5becb082b31f5145fd
tms-nguyenvu <vu.nguyen1@tomosia.com>
2025-04-11 17:28:32 +0700

    fix: ui

--- rails-vite/app/controllers/home_controller.rb
+++ rails-vite/app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @pagy, @posts = pagy(Post.published.recent, limit: 3)
+    @pagy, @posts = pagy(Post.published.recent, limit: 8)
   end
 end
